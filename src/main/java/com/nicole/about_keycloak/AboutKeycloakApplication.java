61cfbc32afabf3649ad1639cc280a5d1b872db48
isun-dev <florachoi1124@naver.com>
2023-10-07 18:51:06 +0900

    Initial commit

---
+++ src/main/java/com/nicole/about_keycloak/AboutKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.nicole.about_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AboutKeycloakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicole/about_keycloak/AboutKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicole.about_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
